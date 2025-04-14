7eab0c9832af026be582e0f20012aff4d659b06d
FOBABS <fobabs2win@rocketmail.com>
2025-04-14 12:31:06 +0100

    fix: replace zero addresses with the right token addresses

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -15,8 +15,8 @@ contract HelperConfig is Script {
     uint256 public constant ANVIL_CHAIN_ID = 31_337;
 
     // Token addresses
-    address public constant USDC_TOKEN_ADDRESS = 0x0000000000000000000000000000000000000000;
-    address public constant SEPOLIA_USDC_TOKEN_ADDRESS = 0x0000000000000000000000000000000000000000;
+    address public constant USDC_TOKEN_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant SEPOLIA_USDC_TOKEN_ADDRESS = 0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8;
 
     constructor() {
         if (block.chainid == ANVIL_CHAIN_ID) {
